5f0059d018e0618f8b7fdb889e9151b6541a1a36
NulledBird <abirdcoder.dev@gmail.com>
2023-11-17 16:15:25 -0800

    Start adding objectives

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <bitset>
+#include <vector>
 
-int generateListOfObjectives(int seconds) {
+std::bitset<32> generateBinary(int seconds) {
     std::bitset<16> binary = std::bitset<16>(seconds).flip();
     binary <<= 2;
     binary = std::bitset<16>(binary.to_ulong() + 1);
@@ -13,14 +14,60 @@ int generateListOfObjectives(int seconds) {
     set1 >>= 1;
     set2 = set2.flip();
 
-    std::bitset<32> finalBitset = set1;
+    std::bitset<32> finalBitset(set1.to_string() + set2.to_string());
 
-    std::cout << binary.to_string() << std::endl;
+    return finalBitset;
+}
+
+int loopThroughObjectives(std::bitset<32> bits) {
+    std::string bitString = bits.to_string();
+
+    std::vector<std::vector<std::string>> objectives;
+
+    int i = 0;
+    for (char c : bitString) {
+        i++;
+
+        if (c == '1') {
+            switch (i)
+            {
+                case 1:
+                    objectives.push_back({"Firewall"});
+                    break;
+
+                case 2:
+                    objectives.push_back({"Chrome"});
+                    break;
+                
+                default:
+                    break;
+            }
+        } else {
+            switch (i)
+            {   
+                default:
+                    break;
+            }
+        }
+
+        
+    }
+
+    for (std::vector<std::string> args: objectives) {
+        std::cout << "{";
+
+        for (std::string arg: args) {
+            std::cout << arg << ",";
+        }
+
+        std::cout << "}" << std::endl;
+    }
 
     return 0;
 }
 
+
 int main() {
-    generateListOfObjectives(37729);
+    loopThroughObjectives(generateBinary(37729));
     return 0;
 }
